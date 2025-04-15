e209f08846e6911ef34d3435add87c75e76f6853
BlackDragon <58775011+BlackDragon6667@users.noreply.github.com>
2025-04-15 09:29:37 +0400

    Update start.php
    order

--- start.php
+++ start.php
@@ -3,12 +3,8 @@ use \Workerman\Worker;
 
 define('GLOBAL_START', true);
 
-// Load classes first
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Start services
+require_once __DIR__ . '/vendor/autoload.php'; // this must come first!
 require_once __DIR__ . '/start_worker.php';
 require_once __DIR__ . '/start_web.php';
 
-// Run all workers
 Worker::runAll();
