81ae2d531f02eaa0d67ee89873128756ae8ec673
BlackDragon <58775011+BlackDragon6667@users.noreply.github.com>
2025-04-15 09:34:04 +0400

    Update start.php
    test3

--- start.php
+++ start.php
@@ -3,8 +3,8 @@ use \Workerman\Worker;
 
 define('GLOBAL_START', true);
 
-require_once __DIR__ . '/vendor/autoload.php'; // this must come first!
 require_once __DIR__ . '/start_worker.php';
 require_once __DIR__ . '/start_web.php';
+
 
 Worker::runAll();
